ddfa53aca3d78358c2b0810ae9b0302046f1ae66
SaurabhKumarECE <saurabhkm304@gmail.com>
2026-04-10 17:18:37 +0530

    Add files via upload

---
+++ Day_1/DataTypes.py
@@ -0,0 +1,7 @@
+a = 10
+b = 3.14
+c = "Hello"
+
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ Day_1/SqandCube.py
@@ -0,0 +1,4 @@
+number = int(input("Enter a number:"))
+
+print("Square of the number is : ", number**2)
+print("Cube of the number is : ", number**3)
