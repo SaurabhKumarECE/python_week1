d77ddc56989a3bae45f931d4b28c279d12154f07
SaurabhKumarECE <saurabhkm304@gmail.com>
2026-05-19 13:03:57 +0530

    Delete Day_1 directory

--- Day_1/DataTypes.py
+++
@@ -1,7 +0,0 @@
-a = 10
-b = 3.14
-c = "Hello"
-
-print(type(a))
-print(type(b))
-print(type(c))
--- Day_1/SqandCube.py
+++
@@ -1,4 +0,0 @@
-number = int(input("Enter a number:"))
-
-print("Square of the number is : ", number**2)
-print("Cube of the number is : ", number**3)
