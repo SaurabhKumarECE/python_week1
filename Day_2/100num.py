116f441b717cfba32d87f94fce7524695e589c8e
SaurabhKumarECE <saurabhkm304@gmail.com>
2026-05-21 10:36:05 +0530

    Add files via upload
    recovered past week deleted asssignment

---
+++ Day_2/100num.py
@@ -0,0 +1,15 @@
+
+
+print("\n---- Print 1 to 100 ----")
+
+# Using for loop
+for i in range(1, 101):
+    print(i, end=" ")
+
+print("\nUsing while loop:")
+
+# Using while loop
+i = 1
+while i <= 100:
+    print(i, end=" ")
+    i += 1
